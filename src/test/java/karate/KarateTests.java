b987dfbe83c223de8a54e7f85f76f080d73b232c
SOULEYSANOGO <SOULEYSANOGO@CIEW15854>
2021-07-10 05:55:17 +0000

    Add  Cucumber Reporting et Parallel

--- src/test/java/karate/KarateTests.java
+++ src/test/java/karate/KarateTests.java
@@ -1,11 +1,44 @@
 package karate;
 
-import com.intuit.karate.junit5.Karate;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.Test;
+
+import com.intuit.karate.Results;
+import com.intuit.karate.Runner;
+
+import net.masterthought.cucumber.Configuration;
+import net.masterthought.cucumber.ReportBuilder;
 
 class KarateTests {
 
-    @Karate.Test
+    /*@Karate.Test
     Karate testAll() {
         return Karate.run().relativeTo(getClass());
+		
+    }*/
+	
+	
+	@Test
+    public void testParallel() {
+        Results results = Runner.parallel(getClass(), 5);
+        generateReport(results.getReportDir());
+        assertTrue(results.getFailCount() == 0);
+    }
+	
+	
+	  public static void generateReport(String karateOutputPath) {
+        Collection<File> jsonFiles = FileUtils.listFiles(new File(karateOutputPath), new String[] { "json" }, true);
+        List<String> jsonPaths = new ArrayList<String>(jsonFiles.size());
+        jsonFiles.forEach(file -> jsonPaths.add(file.getAbsolutePath()));
+        Configuration config = new Configuration(new File("target"), "demo");
+        ReportBuilder reportBuilder = new ReportBuilder(jsonPaths, config);
+        reportBuilder.generateReports();
     }
 }
